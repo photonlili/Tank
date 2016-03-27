98aa83b6e67e2e8b9e906c76fd1a16861f0ec5a8
tianduanrui <tianduanrui@163.com>
2016-03-27 20:30:04 +0800

    进度条240*20

--- HNPrjDemo/main.cpp
+++ HNPrjDemo/main.cpp
@@ -6,36 +6,37 @@ int main(int argc, char *argv[])
 {
     HNApp a(argc, argv);
 
-    HNProgressDialog w;
+    HNFrame w;
+    w.setFixedHeight(340);
+    QVBoxLayout l;
+    w.setLayout(&l);
+    w.show();
+
+    HNProgressDialog w0;
     HNProgressDialog w1;
     HNProgressDialog w2;
-    HNFrame w0;
-    //w0.setFixedHeight(600);
-    QVBoxLayout l;
-    //l.addWidget(&w);
-    //l.addWidget(&w1);
-    //l.addWidget(&w2);
-    w0.setLayout(&l);
-    w0.show();
+    l.addWidget(&w0);
+    l.addWidget(&w1);
+    l.addWidget(&w2);
 
-    int value = 70;
-    w.setFixedWidth(800);
-    w.initAll();
-    w.setValue(value);
+    int value = 100;
+    w0.setFixedWidth(800);
+    w0.initAll();
+    w0.setValue(value);
 
     w1.setFixedWidth(400);
     w1.initAll();
     w1.setValue(value);
 
-    w2.setFixedWidth(600);
+    w2.setFixedWidth(260);
     w2.initAll();
     w2.setValue(value);
 
     HNCloudWidget w3;
-    l.addWidget(&w3);
+    //l.addWidget(&w3);
 
     HNManageEthenetWidget w4;
-    l.addWidget(&w4);
+    //l.addWidget(&w4);
 
     HNInput *im = HNInput::Instance();
     //im->Init("min", "control", "hanon", 14, 14);
